becf2e480a39cd6b8917beee5f75a0d1669e376c
Oksana Oksenyuk <111908245+Oksana-hub2@users.noreply.github.com>
2022-12-11 12:30:16 +0200

    add

---
+++ unite_1_compile/java/level_2/include_libs/src/ua/com/alevel/Ukraine.java
@@ -0,0 +1,8 @@
+package ua.com.alevel;
+
+class Ukraine {
+    public static void main(String[] args) {
+        ua.com.alevel.test.Freedom msg = new ua.com.alevel.test.Freedom();
+        msg.showFreedom();
+    }
+}
