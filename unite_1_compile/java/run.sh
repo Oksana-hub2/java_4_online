a9706731eb56140127fc975fe253112787e057e2
Oksana Oksenyuk <111908245+Oksana-hub2@users.noreply.github.com>
2022-12-11 15:26:31 +0200

    add

--- unite_1_compile/java/run.sh
+++ unite_1_compile/java/run.sh
@@ -1,11 +1,58 @@
+#!
+
+echo 'run level_1'
+
 echo 'run simple'
-cd./simple
+cd ./level_1/simple
 javac Ukraine.java
 java Ukraine
 
-cd../
+cd ../
 
 echo 'run package'
-cd./package
-javac ua/com/alevel/test/Ukraine.java
-java ua.com.alevel.test.Ukraine
+cd ./package
+javac ua/com/alevel/Ukraine.java
+java ua.com.alevel.Ukraine
+
+cd ../
+
+echo 'run separate package'
+cd ./separate_package
+javac ua/com/alevel/Ukraine.java
+java ua.com.alevel.Ukraine
+
+cd ../
+
+echo 'run minimal proj'
+cd ./minimal_proj
+javac -sourcepath ./src -d build/classes ./src/ua/com/alevel/Ukraine.java
+java -cp build/classes ua.com.alevel.Ukraine
+
+cd ../
+
+echo 'run med proj and create simple jar'
+cd ./med_proj
+javac -sourcepath ./src -d build/classes ./src/ua/com/alevel/Ukraine.java
+jar cvfm build/jar/Ukraine.jar resources/MANIFEST.MF -C build/classes .
+java -jar build/jar/Ukraine.jar
+
+cd ../../
+
+echo 'run level 2 (include libs)'
+
+cd ./level_2
+
+echo 'run simple proj who contains external library (jar)'
+cd ./include_libs
+javac -sourcepath ./src -d build/classes/ -cp ./lib/JColor-5.5.1.jar src/ua/com/alevel/util/Message.java src/ua/com/alevel/Hello.java
+java -cp build/classes/:./lib/JColor-5.5.1.jar ua.com.alevel.Hello
+
+cd ../
+
+echo 'run create jar who contains external library (jar)'
+cd ./jar
+. ./run_jar.sh
+
+cd ../../
+
+. ./remove-class.sh
